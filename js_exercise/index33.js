44060ceb082174237cc138c82968c87c6343606b
sidratjahan <sjahan1@my.bcit.ca>
2020-01-29 11:44:11 -0800

    adding exercise 33

---
+++ js_exercise/index33.js
@@ -0,0 +1,27 @@
+// Create a new index33.js file
+// Define the userCount variable and assign the following value: 100
+let userCount = 100;
+// Increment the user count by 5
+userCount = userCount+5;
+// Show the following output: Users count: %userCount%
+console.log(`User count: ${userCount}`);
+
+// Decrement the user count by 3
+userCount = userCount+3;
+
+// Show the following output: Users count: %userCount%
+console.log(`User count: ${userCount}`);
+
+// We bought a new company so now we double the user count
+userCount = 2*userCount;
+
+// Show the following output: Users count: %userCount%
+console.log(`User count: ${userCount}`);
+
+// We know that 50% of the users identify as men and the other 50% identify as women
+let menCount = userCount/2;
+let womenCount = userCount-menCount;
+// Show the following output: Users count: %userCount%, %menCount% % & %womenCount% %
+console.log(`User count: ${userCount}, Men count: ${menCount},Women count: ${womenCount}`);
+
+// Use as many assignment operators as you can
