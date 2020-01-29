5492d5d28a26c1a15d6a95c7620c4c7ec57270d9
sidratjahan <sjahan1@my.bcit.ca>
2020-01-29 11:33:43 -0800

    adding exercise 32

---
+++ js_exercise/index32.js
@@ -0,0 +1,14 @@
+// Create a new index32.js file
+// Define a height variable and assig the following value: 10
+// Define a base variable and assig the following value: 4
+// Calculate the perimiter of the rectangular
+// Calculate the area of the rectangular
+// Show both calculation results, height and base length
+// Use a descriptive message to show all the values
+
+let height = 10;
+let base = 4;
+let perimiter = 2*(height+base);
+let area = height*base;
+let result = `Given height: ${height} and base: ${base} of a rectengular; perimiter and area respectively are ${perimiter} and ${area}`;
+console.log(result);
