28d392a1a49f44aab2df96ced581f2450a70cf9e
sidratjahan <sjahan1@my.bcit.ca>
2020-01-29 12:17:31 -0800

    adding exercise 34

---
+++ js_exercise/index34.js
@@ -0,0 +1,33 @@
+// Create a new index34.js file
+
+// Define the following variables: income, revenue and taxes
+let income, revenue, taxes;
+
+
+// Assign the following values:
+
+// income: 1000
+income = 1000;
+
+// revenue: 600
+revenue = 600;
+
+// taxes: 500
+taxes = 500;
+
+// This months objectives are:
+
+// Income of at least 800 or more
+// Spend less than 400 on taxes
+// We pay bonus if our revenue is 600
+
+// Show the following output (console.log()):
+// Income Objective: true or false (use greater than or equal operator)
+console.log(`Income objective: ${income>=800} `);
+
+
+// Taxes Objective: true or false (use less than operator)
+console.log(`Income objective: ${taxes<400} `);
+
+// Bonus Objective: true or false (use equality operator)
+console.log(`Income objective: ${revenue == 800} `);
