2e1a5603c5b73c614a258eec63ff4376b6f02b3e
sidratjahan <sjahan1@my.bcit.ca>
2020-01-28 22:31:07 -0800

    exercise 30

---
+++ js_exercise/index30.js
@@ -0,0 +1,8 @@
+// Create a new index30.js file
+// Calculate and show the area of a square
+// Side length is 5
+// Show a descriptive message to show the square area and side length
+
+let sideLength = 5;
+let message = `To calculate area of a square, for side length ${sideLength}, area will be ${sideLength*sideLength}`;
+console.log(message);
