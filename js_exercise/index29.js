9d370613203a3ed246121f10f7e55e81a13b58ee
sidratjahan <sjahan1@my.bcit.ca>
2020-01-28 22:20:19 -0800

    exercise 29 added

---
+++ js_exercise/index29.js
@@ -0,0 +1,14 @@
+// Create a new index29.js file
+// Calculate and show the perimeter of a square
+// Side length is 10
+// Show a descriptive message to show the square perimeter and side length
+/*let side_length = 10;
+console.log(`Giver side length of a square ${side_length}, perimeter will be ${4*side_length}`);*/
+
+let sideLength = 10;
+let template = `Given side length of a square ${sideLength}, perimeter will be ${4*sideLength}`;
+console.log(template);
+
+
+
+
