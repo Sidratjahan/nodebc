cb19003e8164a45fed35851f0d025294cba05282
sidratjahan <sjahan1@my.bcit.ca>
2020-01-28 22:39:48 -0800

    exercise 31

---
+++ js_exercise/index31.js
@@ -0,0 +1,13 @@
+// Create a new index31.js file
+// Calculate and show the perimeter of a triangle
+// First side is 10
+// Second side is 20
+// Third side is 5
+// Show a descriptive message to show the triangle perimeter and sides length
+
+let firstSide = 10;
+let secondSide = 20;
+let thirdSide = 5;
+
+let message = `Given three sides of a triangle, ${firstSide}, ${secondSide} and ${thirdSide}; Perimeter will be ${firstSide+secondSide+thirdSide}.`;
+console.log(message);
