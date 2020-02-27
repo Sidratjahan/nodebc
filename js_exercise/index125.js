94791f54905e7e149a880300aa79097139f90abc
sidratjahan <sjahan1@my.bcit.ca>
2020-02-26 18:04:30 -0800

    adding ex126 of javascript

---
+++ js_exercise/index125.js
@@ -0,0 +1,12 @@
+// Define a iceCreamFlavors array
+// Add 10 flavors that you like one by one using the push method
+// Show the iceCreamFlavors items as output
+const iceCreamFlavors = [];
+iceCreamFlavors.push('Lemon');
+iceCreamFlavors.push('Vanila');
+iceCreamFlavors.push('Chocolate');
+iceCreamFlavors.push('Strawberry','Mint','Banana');
+iceCreamFlavors.push('Rainbow Sorbet');
+let arrayLength = iceCreamFlavors.push('Pistachio');
+console.log(iceCreamFlavors);
+console.log(arrayLength);
---
+++ js_exercise/index126.js
@@ -0,0 +1,13 @@
+  
+// Define a iceCreamFlavors array
+// Add 10 flavors that you like one by one using the unshift method
+// Show the iceCreamFlavors items as output
+const iceCreamFlavors = [];
+iceCreamFlavors.unshift('Lemon');
+iceCreamFlavors.unshift('Vanila');
+iceCreamFlavors.unshift('Chocolate');
+iceCreamFlavors.unshift('Strawberry');
+iceCreamFlavors.unshift('Mint','Banana','Melon');
+let arrayLength = iceCreamFlavors.unshift('Pistachio');
+console.log(iceCreamFlavors);
+console.log(arrayLength);
